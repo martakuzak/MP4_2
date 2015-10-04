ba2943e59ca175d3cf1d1bce232f69ea265f97ec
Marta <martakuzak@gmail.com>
2015-10-04 19:07:48 +0200

    Bardzo pomocna dokumentacja

--- Model/Common/bitoperator.h
+++ Model/Common/bitoperator.h
@@ -8,18 +8,58 @@
 #include <qDebug>
 
 class Analyzer;
-
+/**
+ * @brief The BitOperator class performes bit and byte operations
+ */
 class BitOperator {
 public:
     BitOperator();
     ~BitOperator();
-
+    /**
+     * @brief valueOfGroupOfBytes
+     * @param array array of bytes
+     * @param length length of array
+     * @return unsigned long value of the bytes in the array
+     * @example array = {0, 0, 0, 0, 2, 0, 1, 3)
+     * The array represents the number TODO NIE MYŚLĘ
+     * @note unsigned int coding is assumed
+     */
     unsigned long int valueOfGroupOfBytes(const char *array, const unsigned int & length) const;
+    /**
+      TODO
+      JAKIM CUDEM TO MA DZIALAC CO TO JEST????
+      */
+    /**
+     * @brief signedValueOfGroupOfBytes
+     * @param array array of bytes
+     * @param length length of array
+     * @return signed long value of the bytes in the array
+     *
+     */
     signed long int signedValueOfGroupOfBytes(const char *array, const unsigned int & length) const;
+    /**
+     * @brief valueOfGroupOfBits
+     * @param array array of bits
+     * @param length length of array
+     * @return unsigned long value of the bytes in the array
+     * @example array = {0, 0, 0, 1, 0, 0, 1, 0)
+     * The array represents the number 00010010b which is 18. 18 is returned.
+     * @note unsigned int coding is assumed
+     */
     unsigned long int valueOfGroupOfBits(const char *array, const unsigned int & length) const;
+    /**
+     * @brief stringValue
+     * @param entry array that represents the bytes
+     * @param length length of the array
+     * @return string value created by combining the bytes together
+     * @example entry = {77, 64, 114, 99, 105, 110}
+     * The result is the combination of each entry character converted to the ASCII character: M@arcin
+     * @note ASCII coding used here
+     */
     QString stringValue(const char *entry, const unsigned int & length) const;
 
     //DO WYRZUCENIA STĄD
+    //TODO - CO Z TYM ZROBIC?
     unsigned long int unsignedExpGolomb(QFile *file, const unsigned long int& offset, int* resLength) const;
 };
 
--- Model/Common/fileservice.h
+++ Model/Common/fileservice.h
@@ -7,11 +7,31 @@
 #include <QString>
 #include <QDebug>
 
+/**
+ * @brief The FileService class is destined to do basic operations on the file
+ */
 class FileService {
 public:
+    /**
+     * @brief FileService
+     * @param name file name
+     */
     FileService(const QString &name);
     ~FileService();
 
+    /**
+     * @brief openFile Opens file with the name given in constructor paramater
+     * @param openMode open mode
+     * QIODevice::NotOpen	0x0000	The device is not open.
+        QIODevice::ReadOnly	0x0001	The device is open for reading.
+        QIODevice::WriteOnly	0x0002	The device is open for writing.
+        QIODevice::ReadWrite	ReadOnly | WriteOnly	The device is open for reading and writing.
+        QIODevice::Append	0x0004	The device is opened in append mode, so that all data is written to the end of the file.
+        QIODevice::Truncate	0x0008	If possible, the device is truncated before it is opened. All earlier contents of the device are lost.
+        QIODevice::Text	0x0010	When reading, the end-of-line terminators are translated to '\n'. When writing, the end-of-line terminators are translated to the local encoding, for example '\r\n' for Win32.
+        QIODevice::Unbuffered	0x0020	Any buffer in the device is bypassed.
+     * @return true if file opens with success
+     */
     bool openFile(QIODevice::OpenMode openMode = QIODevice::ReadOnly) {
         if(file == NULL)
             qDebug()<<"FILESERVICE: openFile file is null";
@@ -19,15 +39,51 @@ public:
         qDebug()<<"FILESERVICE: openFile file open? : "<<tmp;
         return tmp;
     }
+    /**
+     * @brief getSize
+     * @return size of the file
+     */
     qint64 getSize() { return file->size(); }
+    /**
+     * @brief close closes the file
+     */
     void close() { file->close(); }
     //offset i number w byte'ach
+    /**
+     * @brief getBytes
+     * @param length length of the byte array in bytes
+     * @param offset offset in file (in bytes)
+     * @return char array containing the bytes of given length which are located in the file with given offset
+     */
     char* getBytes(unsigned int length, unsigned long offset);
     //offset i number w bitach
+    /**
+     * @brief getBits
+     * @param length length of the bit array in bits
+     * @param offset offset in file (in bits)
+     * @return char array containing the bits of given length which are located in the file with given offset
+     */
     char* getBits(unsigned int length, unsigned long offset);
 protected:
+    /**
+     * @brief toBitArray converts byte into char bit array
+     * @param byte byte that contains bit sequence that is supposed to be returned
+     * @param prefix number of bits to skip as a prefix (MSB)
+     * @param suffix number of bits to skip as a suffix (LSB)
+     * @example Let's say that char* tmp = toBitArray(254, 3, 1);
+     * 254 -> 11111110b
+     * We skip 3 MSB and 1 LSB, so the result is 1111b
+     * tmp = {1, 1, 1, 1}
+     * @return
+     */
     char* toBitArray(char byte, int prefix = 0, int suffix = 0);
+    /**
+     * @brief file file on which the class operates
+     */
     QFile* file;
+    /**
+     * @brief BITS_IN_BYTE number of bits in the byte
+     */
     const int BITS_IN_BYTE = 8;
 };
 
